59e87ecd0c81341a3843cd8f576860c2ef0240c4
田島慎司 <tajimashinji@tajimashinjinoMacBook-Pro.local>
2020-03-04 22:27:55 +0100

    hedderデザイン草案

---
+++ header.js
@@ -0,0 +1,6 @@
+var path = location.pathname;
+var obj = null;
+if (path == "/Users/tajimashinji/Documents/Workbench/web/WebDevelopment/index-ja.html") {
+    obj = document.getElementById("header-menu-index");
+}
+obj.style.backgroundColor="色"
