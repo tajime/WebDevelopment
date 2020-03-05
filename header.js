c9caa15806c9394f2290069275112a4ce6cfde62
田島慎司 <tajimashinji@tajimashinjinoMacBook-Pro.local>
2020-03-05 10:24:39 +0100

    headerほぼ完成

--- header.js
+++ header.js
@@ -1,6 +1,9 @@
 var path = location.pathname;
 var obj = null;
+var img = document.getElementById("header-menu-maru");
 if (path == "/Users/tajimashinji/Documents/Workbench/web/WebDevelopment/index-ja.html") {
     obj = document.getElementById("header-menu-index");
 }
-obj.style.backgroundColor="色"
+
+//色の指定
+obj.style.backgroundColor="#fffacd"
